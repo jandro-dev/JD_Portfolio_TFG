20d6ff06972635fbc5b1a94e6b38eb297e6565d5
afrancop05 <jandro.develop@gmail.com>
2025-01-31 19:15:08 +0100

    Add: Config CORS

---
+++ backend/src/main/java/jandrodev/portfolio/backend/CorsConfig.java
@@ -0,0 +1,18 @@
+package jandrodev.portfolio.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registro) {
+        registro.addMapping("/api/**")
+                .allowedOrigins("**")
+                .allowedMethods("GET")
+                .allowedHeaders("*");
+    }
+
+}
