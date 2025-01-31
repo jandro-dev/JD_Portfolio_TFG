1bb69b51df75a0ea1d6879182c443abb5d3357c6
afrancop05 <jandro.develop@gmail.com>
2025-01-31 18:10:01 +0100

    Add: Conexion a BD securizada

--- backend/src/main/java/jandrodev/portfolio/backend/BackendApplication.java
+++ backend/src/main/java/jandrodev/portfolio/backend/BackendApplication.java
@@ -3,8 +3,20 @@ package jandrodev.portfolio.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
+
+	static {
+        // Cargar variables del archivo .env al entorno
+        Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
