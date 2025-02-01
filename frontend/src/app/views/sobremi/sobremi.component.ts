d5c091d296cef013f7935731983a02e0010471bb
afrancop05 <jandro.develop@gmail.com>
2025-02-01 18:14:47 +0100

    Update: Vista sobremi hecha

--- frontend/src/app/views/sobremi/sobremi.component.ts
+++ frontend/src/app/views/sobremi/sobremi.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ContactoComponent } from '../../components/contacto/contacto.component';
+import { BotonComponent } from '../../components/boton/boton.component';
+import { SkillCardComponent } from '../../components/skill-card/skill-card.component';
 
 @Component({
-  selector: 'app-sobremi',
+  selector: 'vistaSobreMi',
   standalone: true,
-  imports: [],
+  imports: [ContactoComponent, BotonComponent, SkillCardComponent],
   templateUrl: './sobremi.component.html',
-  styleUrl: './sobremi.component.css'
+  styleUrl: './sobremi.component.css',
 })
-export class SobremiComponent {
-
-}
+export class SobremiComponent {}
