4a76d26bccc9c3bdf48c019f25e76b3cb0f8664b
afrancop05 <jandro.develop@gmail.com>
2025-02-01 19:18:35 +0100

    Update: Vista inicio hecha

--- frontend/src/app/views/inicio/inicio.component.ts
+++ frontend/src/app/views/inicio/inicio.component.ts
@@ -1,12 +1,173 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { BotonComponent } from '../../components/boton/boton.component';
+import { LogocardComponent } from '../../components/logocard/logocard.component';
+import { CarruselComponent } from '../../components/carrusel/carrusel.component';
+import { ContactoComponent } from '../../components/contacto/contacto.component';
 
 @Component({
-  selector: 'app-inicio',
+  selector: 'vistaInicio',
   standalone: true,
-  imports: [],
+  imports: [
+    HttpClientModule,
+    CommonModule,
+    BotonComponent,
+    LogocardComponent,
+    CarruselComponent,
+    ContactoComponent,
+  ],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+  // Variables
+  titulo: string = 'Aplicaciones android';
+  textoSkill: string =
+    'Hago aplicaciones para los sistemas operativos de Google. Creo apps en Android para móviles usando Kotlin y Jetpack Compose.';
+  textos: string[] = [
+    'Mi nombre es Alejandro Franco',
+    'Soy desarrollador full stack',
+    'creo aplicaciones',
+    'moviles | web',
+    'y diseño logotipos',
+  ];
+  part: string = '';
+  i: number = 0;
+  desplazamiento: number = 0;
+  sizeTextos: number = this.textos.length;
+  avanzar: boolean = true;
+  skip_count: number = 0;
+  skip_delay: number = 15;
+  velAnimacion: number = 60;
 
+  constructor(private referencia: ElementRef) {}
+
+  // Inicializaciones
+  ngOnInit(): void {
+    this.actualizarTextoAnimado();
+
+    // Cambia la informacion segun la opcion escogida
+    document.addEventListener('DOMContentLoaded', () => {
+      let opcionHabilidad = document.querySelectorAll('.opcion');
+      let imagen = document.querySelector('.cambiarImg') as HTMLImageElement;
+      let icono = document.querySelector('.cambiarSkill') as HTMLImageElement;
+
+      Array.prototype.forEach.call(opcionHabilidad, (div: HTMLElement) => {
+        div.addEventListener('click', () => {
+          let link = div.querySelector('span');
+
+          // Eliminar la clase clicked de todos los elementos
+          Array.prototype.forEach.call(
+            opcionHabilidad,
+            (element: HTMLElement) => {
+              element.classList.remove('clicked');
+            }
+          );
+
+          // Agregar la clase clicked solo al elemento clickeado
+          div.classList.add('clicked');
+
+          // Modifica el contenido segun la opcion elegida
+          switch (link?.textContent) {
+            case 'App':
+              this.titulo = 'Aplicaciones android';
+              this.textoSkill =
+                'Hago aplicaciones para los sistemas operativos de Google. Creo apps en Android para móviles usando Kotlin y Jetpack Compose.';
+              imagen.src = 'assets/skills/app.webp';
+              icono.src = 'assets/skills/appSkill.svg';
+              break;
+
+            case 'Web':
+              this.titulo = 'Páginas web y backend';
+              this.textoSkill =
+                'Desarrollo y mantengo páginas web con Angular y NodeJS. También creo Back-End utilizando Spring MVC y MySQL.';
+              imagen.src = 'assets/skills/web.webp';
+              icono.src = 'assets/skills/webSkill.svg';
+              break;
+
+            case 'Diseño':
+              this.titulo = 'Diseño gráfico';
+              this.textoSkill =
+                'Realizo marcas y logotipos usando Photoshop. El diseño lo aplico además en aplicaciones móviles y páginas web utilizando Figma.';
+              imagen.src = 'assets/skills/design.webp';
+              icono.src = 'assets/skills/designSkill.svg';
+              break;
+
+            default:
+              this.titulo = '';
+              this.textoSkill = '';
+              imagen.src = '';
+              icono.src = '';
+              break;
+          }
+        });
+      });
+    });
+  }
+
+  // Escritura en movimiento: https://ideakreativa.net/efectos-de-animacion-de-texto-css-ejemplos/
+  actualizarTextoAnimado(): void {
+    setInterval(() => {
+      // Comprueba si la animacion continua hacia delante
+      if (this.avanzar) {
+        // Si ha terminado el texto suma un contador para esperar a borrar el texto
+        if (this.desplazamiento >= this.textos[this.i].length) {
+          this.skip_count++;
+          if (this.skip_count === this.skip_delay) {
+            this.avanzar = false;
+            this.skip_count = 0;
+          }
+        }
+
+        /* La animacion va hacia atras ya que el contador de saltos ahora es 0 y 
+        cuando el desplazamiento sea 0 coge el siguiente texto del array */
+      } else {
+        if (this.desplazamiento === 0) {
+          this.avanzar = true;
+          this.i++;
+
+          // Reinicio del array cuando lo termina
+          if (this.i >= this.sizeTextos) {
+            this.i = 0;
+          }
+        }
+      }
+
+      // Coge el texto desde el primer caracter hasta donde este el desplazamiento
+      this.part = this.textos[this.i].substring(0, this.desplazamiento);
+
+      /* Si los saltos son 0 y la animacion es adelante incrementa 
+          el indice de desplazamiento sino lo disminuye */
+      if (this.skip_count === 0) {
+        if (this.avanzar) {
+          this.desplazamiento++;
+        } else {
+          this.desplazamiento--;
+        }
+      }
+
+      // Actualiza la clase texto con lo que hay en part
+      const wordElement = document.querySelector('.textoAnimado');
+      if (wordElement) {
+        wordElement.textContent = this.part;
+      }
+    }, this.velAnimacion);
+  }
+
+  // Funcion para redireccionar al id perfil sin fijarlo en la url
+  scrollToPerfil(event: Event) {
+    event.preventDefault();
+    const perfilElement =
+      this.referencia.nativeElement.querySelector('#perfil');
+    if (perfilElement) {
+      perfilElement.scrollIntoView({ block: 'start' });
+    }
+  }
+
+  // Funcion para enlace en componente boton
+  abrirNuevaVentana(event: Event, url: string) {
+    event.preventDefault();
+    window.open(url, '_blank');
+  }
 }
