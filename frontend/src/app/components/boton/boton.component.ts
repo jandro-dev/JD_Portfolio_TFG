87e4862710fc1c5c5bf24b3ca290889ee7c1703a
afrancop05 <jandro.develop@gmail.com>
2025-02-01 12:55:04 +0100

    Update: Componente boton hecho

--- frontend/src/app/components/boton/boton.component.ts
+++ frontend/src/app/components/boton/boton.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-boton',
+  selector: 'componenteBoton',
   standalone: true,
   imports: [],
   templateUrl: './boton.component.html',
-  styleUrl: './boton.component.css'
+  styleUrl: './boton.component.css',
 })
 export class BotonComponent {
+  // Entradas por defecto
+  @Input() texto: string = 'Boton';
+  @Input() colorBoton: string = '#000';
+  @Input() colorHover: string = '#fff';
+  @Input() url: string = '';
 
+  isHover: boolean = false;
+
+  entradaHover() {
+    this.isHover = true;
+  }
+
+  salidaHover() {
+    this.isHover = false;
+  }
 }
