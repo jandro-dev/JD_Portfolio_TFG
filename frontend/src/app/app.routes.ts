0a21adbe5b2ec987d91bdc739ea8c1a4e1020be2
afrancop05 <jandro.develop@gmail.com>
2025-02-01 18:02:34 +0100

    Add: Rutas vinculadas a vistas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { SobremiComponent } from './views/sobremi/sobremi.component';
+import { PortfolioComponent } from './views/portfolio/portfolio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { SetupComponent } from './views/setup/setup.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent,
+  },
+  {
+    path: 'jandrodev',
+    component: SobremiComponent,
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent,
+  },
+  {
+    path: 'setup',
+    component: SetupComponent,
+  },
+  {
+    path: 'notfound',
+    component: NotfoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'notfound',
+  },
+];
