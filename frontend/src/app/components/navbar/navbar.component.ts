b4358a90236a19ad7579ff0eb1e9c317ce43d7a7
afrancop05 <jandro.develop@gmail.com>
2025-02-01 17:41:24 +0100

    Add: Componente navbar hecho

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'componenteNavbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isMenuMovil = true;
 
+  // Renderer manipula el DOM de forma segura
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.cambiarNavbar();
+  }
+
+  // Añade clase scrolled a etiqueta navbar si la ventana se ha scrolleado verticalmente > 10px
+  cambiarNavbar() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 10) {
+      this.renderer.addClass(navbar, 'scrolled');
+    } else {
+      this.renderer.removeClass(navbar, 'scrolled');
+    }
+  }
+
+  // Evento para saber cuando se scrollea la pantalla
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.cambiarNavbar();
+  }
+
+  // Funcion para entrada / salida menu movil
+  alternarMenu() {
+    this.isMenuMovil = !this.isMenuMovil;
+  }
 }
