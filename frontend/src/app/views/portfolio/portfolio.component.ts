0556d77cb5e6f5f6e6110a4993c044ca59d6f4ef
afrancop05 <jandro.develop@gmail.com>
2025-02-02 16:27:26 +0100

    Update: Vista portfolio hecha

--- frontend/src/app/views/portfolio/portfolio.component.ts
+++ frontend/src/app/views/portfolio/portfolio.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactoComponent } from '../../components/contacto/contacto.component';
+import { ParallaxCardComponent } from '../../components/parallax-card/parallax-card.component';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
 
 @Component({
-  selector: 'app-portfolio',
+  selector: 'vistaPortfolio',
   standalone: true,
-  imports: [],
+  imports: [ContactoComponent, ParallaxCardComponent, CommonModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css'
+  styleUrl: './portfolio.component.css',
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+  proyectos: any;
+  disenios: any;
 
+  constructor(private apiService: ApiService) {}
+
+  // Inicializa portfolio y disenios cogiendo los datos de la api
+  ngOnInit(): void {
+    this.apiService.getProyectos().subscribe((data) => {
+      this.proyectos = data;
+    });
+    this.apiService.getDisenios().subscribe((data) => {
+      this.disenios = data;
+    });
+  }
 }
